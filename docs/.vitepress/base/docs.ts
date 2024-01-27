10d754bfa368155a17936737c0a5652633a96da2
liangchuying <383696165@qq.com>
2024-01-27 11:21:03 +0800

    feat: first

---
+++ docs/.vitepress/base/docs.ts
@@ -0,0 +1,27 @@
+import { enUSConfig, zhCNConfig } from "../configs";
+
+export const docsConfig = {
+    base: "/",
+    title: "梦和远方",
+    description: "梦和远方的博客",
+    lang: "zh-CN",
+    lastUpdated: true,
+    /* 语言配置 */
+    locales: {
+        // 若果配置了root，则双击title的时候不会返回/路径下了，只会返回在link路径下
+        // root: { label: "简体中文", lang: "zh-CN", link: "/zh-CN",  ...zhCNConfig },
+        "zh-CN": {
+            label: "简体中文",
+            lang: "zh-CN",
+            link: "/zh-CN", // index 直接跳转到首页
+            ...zhCNConfig,
+        },
+        "en-US": {
+            label: "English",
+            lang: "en-US",
+            link: "/en-US",
+            ...enUSConfig,
+        },
+    },
+};
+
---
+++ docs/.vitepress/base/head.ts
@@ -0,0 +1,6 @@
+import type { HeadConfig } from "vitepress";
+
+export const head: HeadConfig[] = [
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["meta", { name: "keywords", content: "梦和远方, 梦和远方的博客" }],
+];
